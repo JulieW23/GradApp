d2a6978caa5848333fad971f30e69343d3938c68
George Lam <georgelam@George.local>
2016-11-07 12:21:55 -0500

    Added a teacher's initial login screen

--- Kiddo/app/src/main/java/com/team_05/kiddo/MainActivity.java
+++ Kiddo/app/src/main/java/com/team_05/kiddo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +19,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                // If login is admin
+
+                // If login is teacher
+
+                // If login is parent
+
+                // Temp login
+                // Switch to whatever activity you want to test it on
+                Intent intent = new Intent(MainActivity.this, TeacherInitialLoginActivity.class);
+                startActivity(intent);
             }
         });
     }
+
+
 }
