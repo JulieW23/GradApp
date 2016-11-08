0d276a52c275d4aa06251841cb0d3bc8d98935fe
George Lam <georgelam@George.local>
2016-11-07 23:59:04 -0500

    Added settingsActivity

--- Kiddo/app/src/main/java/com/team_05/kiddo/ClassesActivity.java
+++ Kiddo/app/src/main/java/com/team_05/kiddo/ClassesActivity.java
@@ -1,7 +1,12 @@
 package com.team_05.kiddo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class ClassesActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class ClassesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
+
+        // Settings button®
+        ImageButton settingsImageButton = (ImageButton)(findViewById(R.id.settingsImageButton));
+        settingsImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClassesActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ Kiddo/app/src/main/java/com/team_05/kiddo/SettingsActivity.java
@@ -0,0 +1,27 @@
+package com.team_05.kiddo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Back button
+        ImageButton backImageButton = (ImageButton)(findViewById(R.id.backImageButton));
+        backImageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+}
