7988e3e97b0bdc791a49abdeffd17761048b17b0
George Lam <georgelam@George.local>
2016-11-09 21:34:54 -0500

    Fixed some of the tabs

---
+++ Kiddo/app/src/main/java/com/team_05/kiddo/CalendarTab.java
@@ -0,0 +1,30 @@
+package com.team_05.kiddo;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.util.Date;
+import java.util.Locale;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+
+
+public class CalendarTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
