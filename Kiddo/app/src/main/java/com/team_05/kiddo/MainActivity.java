4f682382fc8c0fe31c92548ed04fb785aa35fff9
Julie Wang <juwang321@gmail.com>
2016-11-12 13:59:47 -0500

    add child screen done, course enrolment screen almost done, edited terms and conditions

--- Kiddo/app/src/main/java/com/team_05/kiddo/MainActivity.java
+++ Kiddo/app/src/main/java/com/team_05/kiddo/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 // Temp login
                 // Switch to whatever activity you want to test it on
                 Intent intent = new Intent(MainActivity.this, ChildrenActivity.class);
+                //Intent intent = new Intent(MainActivity.this, TeacherInitialLoginActivity.class);
                 finish();
                 startActivity(intent);
             }
