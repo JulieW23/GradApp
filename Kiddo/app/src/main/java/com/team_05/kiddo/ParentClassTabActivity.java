464c6bcea94f1c5341b0bc1612b3337d90d9096b
Julie Wang <juwang321@gmail.com>
2016-11-13 02:03:38 -0500

    added tabs

--- Kiddo/app/src/main/java/com/team_05/kiddo/ParentClassTabActivity.java
+++ Kiddo/app/src/main/java/com/team_05/kiddo/ParentClassTabActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TabHost;
 
 public class ParentClassTabActivity extends AppCompatActivity {
 
@@ -12,6 +13,24 @@ public class ParentClassTabActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_class_tab);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.pcTabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1 = tabHost.newTabSpec("announcements");
+        spec1.setContent(R.id.pannouncementsTab);
+        spec1.setIndicator("Announcements");
+        tabHost.addTab(spec1);
+
+        TabHost.TabSpec spec2 = tabHost.newTabSpec("class_updates");
+        spec2.setContent(R.id.classUpdatesTab);
+        spec2.setIndicator("Class Updates");
+        tabHost.addTab(spec2);
+
+        TabHost.TabSpec spec3 = tabHost.newTabSpec("pmessages");
+        spec3.setContent(R.id.pMessagesTab);
+        spec3.setIndicator("Messages");
+        tabHost.addTab(spec3);
 
         // Settings button
         ImageButton settingsImageButton = (ImageButton)(findViewById(R.id.settingsImageButton));
