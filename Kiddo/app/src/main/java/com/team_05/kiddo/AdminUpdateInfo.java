8a66ae5e59568b3b271994ad4a1e956ae68b03d7
Shirley Ng <sn121@users.noreply.github.com>
2016-12-06 01:26:38 -0500

    added update info activity for admin

---
+++ Kiddo/app/src/main/java/com/team_05/kiddo/AdminUpdateInfo.java
@@ -0,0 +1,13 @@
+package com.team_05.kiddo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminUpdateInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_update_info);
+    }
+}
